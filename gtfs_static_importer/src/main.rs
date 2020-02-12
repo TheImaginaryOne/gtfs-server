208c56c100cef243aa7838b4097b825cd5b5b73f
TheImaginaryOne <johnathanleung@hotmail.co.nz>
2020-02-12 18:39:49 +1300

    Add initial code for GTFS static feed download
    Also change GTFS importer to async code

--- gtfs_static_importer/src/main.rs
+++ gtfs_static_importer/src/main.rs
@@ -1,13 +1,18 @@
+use bytes::buf::BufMutExt;
+use bytes::{Bytes, BytesMut};
 use dotenv::dotenv;
-use postgres::{Client, NoTls};
+use futures_util::sink::SinkExt;
+use tokio::io::AsyncWriteExt;
+use tokio_postgres::{Client, NoTls};
 
 use std::error::Error;
-use std::io::Write;
 use std::path::Path;
 
 use structopt::StructOpt;
 
 use derive_more::{Display, From};
+
+use indicatif::{ProgressBar, ProgressStyle};
 
 static TABLE_AND_FILE_NAMES: [(&str, &str); 8] = [
     ("shapes.txt", "shape"),
@@ -23,7 +28,7 @@ static TABLE_AND_FILE_NAMES: [(&str, &str); 8] = [
 #[derive(Debug, From, Display)]
 enum ImporterError {
     #[display(fmt = "Database error: {}", _0)]
-    DbError(postgres::Error),
+    DbError(tokio_postgres::Error),
     #[display(fmt = "File error: {}", _0)]
     FileError(std::io::Error),
     #[display(fmt = "{} file should have data", _0)]
@@ -32,6 +37,12 @@ enum ImporterError {
     CsvError(csv::Error),
     #[display(fmt = "Error reading env var {}: {}", _0, _1)]
     EnvVar(String, std::env::VarError),
+    #[display(fmt = "Http error: {}", _0)]
+    Http(reqwest::Error),
+    #[display(fmt = "Zip extraction error: {}", _0)]
+    Zip(zip::result::ZipError),
+    #[display(fmt = "Tokio error: {}", _0)]
+    TokioJoin(tokio::task::JoinError),
 }
 impl Error for ImporterError {}
 
@@ -42,76 +53,158 @@ enum Options {
         #[structopt(short, long)]
         path: String,
     },
+    Download {
+        #[structopt(short = "f", long)]
+        tf_feed_id: String,
+    },
     DeleteFeed {
-        #[structopt(short="f", long)]
+        #[structopt(short = "f", long)]
         feed_id: u32,
-    }
+    },
 }
-fn main() {
-    match run() {
+#[tokio::main]
+async fn main() {
+    match run().await {
         Ok(()) => println!("Successful!"),
         Err(e) => eprintln!("{}", e),
     }
 }
-fn run() -> Result<(), ImporterError> {
+async fn run() -> Result<(), ImporterError> {
     let options = Options::from_args();
 
     dotenv().ok();
 
-    let db_url = &std::env::var("DATABASE_URL").map_err(|e| ImporterError::EnvVar("DATABASE_URL".into(), e))?;
+    let db_url = &std::env::var("DATABASE_URL")
+        .map_err(|e| ImporterError::EnvVar("DATABASE_URL".into(), e))?;
 
     println!("Connecting to {}", db_url);
-    let mut client = Client::connect(db_url, NoTls)?;
+    let (mut client, connection) = tokio_postgres::connect(db_url, NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
 
     match options {
-        Options::Import {path} => import(path, &mut client),
-        Options::DeleteFeed {feed_id} => delete_feed(feed_id, &mut client)
+        Options::Import { path } => import(path, &mut client).await,
+        Options::DeleteFeed { feed_id } => delete_feed(feed_id, &mut client).await,
+        Options::Download { tf_feed_id } => download(tf_feed_id, &mut client).await,
     }
 }
-
-fn delete_feed(feed_id: u32, client: &mut Client) -> Result<(), ImporterError> {
-    let mut transaction = client.transaction()?;
+// todo async
+async fn delete_feed(feed_id: u32, client: &mut Client) -> Result<(), ImporterError> {
+    let transaction = client.transaction().await?;
     println!("Deleting data with feed_id = {}", feed_id);
 
     // rev to avoid foreign key violations
     for s in TABLE_AND_FILE_NAMES.iter().rev() {
         println!("Deleting from table {}", &s.1);
-        transaction.execute(
-            &format!("delete from {} where feed_id={}", &s.1, &feed_id)[..],
-            &[],
-        )?;
+        transaction
+            .execute(
+                &format!("delete from {} where feed_id={}", &s.1, &feed_id)[..],
+                &[],
+            )
+            .await?;
     }
     println!("Deleting from table feed");
-    transaction.execute(
-        &format!("delete from feed where feed_id={}", &feed_id)[..],
-        &[],
-    )?;
+    transaction
+        .execute(
+            &format!("delete from feed where feed_id={}", &feed_id)[..],
+            &[],
+        )
+        .await?;
 
-    transaction.commit()?;
+    transaction.commit().await?;
     Ok(())
 }
 
-fn import(p: String, client: &mut Client) -> Result<(), ImporterError> {
+async fn download(feed_id: String, _client: &mut Client) -> Result<(), ImporterError> {
+    let tf_key = &std::env::var("TRANSITFEEDS_KEY")
+        .map_err(|e| ImporterError::EnvVar("TRANSITFEEDS_KEY".into(), e))?;
+
+    let mut async_file = tokio::task::spawn_blocking(|| {
+        let temp_file = tempfile::tempfile()?;
+        Ok::<tokio::fs::File, ImporterError>(tokio::fs::File::from_std(temp_file))
+    })
+    .await??;
+
+    println!("Downloading latest feed");
+
+    let client = reqwest::Client::new();
+
+    let mut response = client
+        .get("https://api.transitfeeds.com/v1/getLatestFeedVersion")
+        .query(&[("key".to_string(), tf_key), ("feed".to_string(), &feed_id)])
+        .send()
+        .await?;
+
+    if let Some(l) = response.content_length() {
+        let bar = ProgressBar::new(l);
+        bar.set_style(ProgressStyle::default_bar().progress_chars("=> ").template(
+            "Downloading {spinner} [{elapsed_precise}] [{bar:60.yellow}] {bytes}/{total_bytes}",
+        ));
+
+        while let Some(chunk) = response.chunk().await? {
+            bar.inc(chunk.len() as u64);
+            async_file.write(&chunk).await?;
+        }
+    }
+
+    // unwrap should work, as we have finished all async io operations.
+    let mut zip = zip::ZipArchive::new(async_file.try_into_std().unwrap())?;
+
+    let temp_folder = tempfile::tempdir()?;
+    let temp_folder_path = temp_folder.path();
+
+    let bar = ProgressBar::new(zip.len() as u64);
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .progress_chars("=> ")
+            .template("Writing {spinner} [{elapsed_precise}] [{bar:60.yellow}] {pos}/{len}"),
+    );
+
+    for i in 0..zip.len() {
+        let mut inner = zip.by_index(i)?;
+
+        let file_name = inner.sanitized_name();
+
+        bar.println(format!("Writing {}", file_name.display()));
+
+        let file_path = std::path::PathBuf::from(temp_folder_path).join(file_name);
+        let mut new_file = std::fs::File::create(file_path)?;
+        std::io::copy(&mut inner, &mut new_file)?;
+
+        bar.inc(1);
+    }
+
+    Ok(())
+}
+
+async fn import(p: String, client: &mut Client) -> Result<(), ImporterError> {
     let path = Path::new(&p);
 
     println!("Importing data");
 
-    let mut transaction = client.transaction()?;
+    let transaction = client.transaction().await?;
 
     let feed_id: i32 = transaction
-        .query("insert into feed default values returning feed_id", &[])?
+        .query("insert into feed default values returning feed_id", &[])
+        .await?
         .first()
         .unwrap()
         .get(0);
 
     for s in &TABLE_AND_FILE_NAMES {
-        transaction.execute(
-            &format!(
-                "alter table {} alter column feed_id set default {}",
-                s.1, feed_id
-            )[..],
-            &[],
-        )?;
+        transaction
+            .execute(
+                &format!(
+                    "alter table {} alter column feed_id set default {}",
+                    s.1, feed_id
+                )[..],
+                &[],
+            )
+            .await?;
 
         let file_path = path.join(&s.0);
         println!("Reading from {}", &file_path.display());
@@ -142,52 +235,55 @@ fn import(p: String, client: &mut Client) -> Result<(), ImporterError> {
         );
         println!("Running: {}", &command);
 
-        let mut writer = transaction.copy_in(&command[..])?;
+        let writer = transaction.copy_in(&command[..]).await?;
+        futures::pin_mut!(writer);
 
         println!("Writing data");
 
         if s.1 == "stop_time" {
-            let mut reader = csv::Reader::from_reader(file_content.as_bytes());
-            let mut csv_writer = csv::Writer::from_writer(&mut writer);
-            let header_columns = header.split(',').collect::<Vec<_>>();
-            let arrival_time_idx = header_columns
-                .iter()
-                .position(|r| *r == "arrival_time");
-            let departure_time_idx = header_columns
-                .iter()
-                .position(|r| *r == "departure_time");
+            let mut buffer = BytesMut::new().writer();
+            {
+                let mut reader = csv::Reader::from_reader(file_content.as_bytes());
+                let mut csv_writer = csv::Writer::from_writer(&mut buffer);
+                let header_columns = header.split(',').collect::<Vec<_>>();
+                let arrival_time_idx = header_columns.iter().position(|r| *r == "arrival_time");
+                let departure_time_idx = header_columns.iter().position(|r| *r == "departure_time");
 
-            for row in reader.records() {
-                let record = row?; // todo, ugly code!
-                let new_record = record
-                    .iter()
-                    .enumerate()
-                    .map(|(i, content)| {
-                        if Some(i) == arrival_time_idx || Some(i) == departure_time_idx {
-                            let x = content
-                                .split(':')
-                                .map(|x| x.parse::<i32>().unwrap())
-                                .collect::<Vec<i32>>();
-                            (x[0] * 3600 + x[1] * 60 + x[2]).to_string()
-                        } else {
-                            content.to_string()
-                        }
-                    })
-                    .collect::<Vec<String>>();
-                csv_writer.write_record(new_record)?;
+                for row in reader.records() {
+                    let record = row?; // todo, ugly code!
+                    let new_record = record
+                        .iter()
+                        .enumerate()
+                        .map(|(i, content)| {
+                            if Some(i) == arrival_time_idx || Some(i) == departure_time_idx {
+                                let x = content
+                                    .split(':')
+                                    .map(|x| x.parse::<i32>().unwrap())
+                                    .collect::<Vec<i32>>();
+                                (x[0] * 3600 + x[1] * 60 + x[2]).to_string()
+                            } else {
+                                content.to_string()
+                            }
+                        })
+                        .collect::<Vec<String>>();
+                    csv_writer.write_record(new_record)?;
+                }
             }
+            writer.send(buffer.into_inner().freeze()).await?;
         } else {
-            writer.write_all(file_content.as_bytes())?;
+            writer.send(Bytes::from(file_content)).await?;
         }
         println!("Committing to database");
 
-        writer.finish()?;
+        writer.finish().await?;
 
-        transaction.execute(
-            &format!("alter table {} alter column feed_id drop default", s.1)[..],
-            &[],
-        )?;
+        transaction
+            .execute(
+                &format!("alter table {} alter column feed_id drop default", s.1)[..],
+                &[],
+            )
+            .await?;
     }
-    transaction.commit()?;
+    transaction.commit().await?;
     Ok(())
 }
